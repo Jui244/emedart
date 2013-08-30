a9549399e3aad5394d94fe88397bd580ac43c2c6
Josianne Hyson <Josianne.H@hotmail.com>
2013-08-31 11:14:36 +1200

    Made google maps show up :D

--- d3trademe/app/assets/javascripts/application.js
+++ d3trademe/app/assets/javascripts/application.js
@@ -18,8 +18,8 @@ var map;
 function initialize(){
 
 	var mapOptions = {
-		zoom: 8,
-		center: new google.maps.LatLng(-34.397, 150.644),
+		zoom: 6,
+		center: new google.maps.LatLng(-41.274001, 174.770494),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
