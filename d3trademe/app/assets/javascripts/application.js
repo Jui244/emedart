ae0bc4bdfbcfafc1dfe96602336e29731c03b1d1
maxdgodfrey <maxdgodfrey@gmail.com>
2013-08-31 10:53:41 +1200

    added some test google maps code.

--- d3trademe/app/assets/javascripts/application.js
+++ d3trademe/app/assets/javascripts/application.js
@@ -14,4 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-window.console.log("Hello, world");
+var map;
+function initialize(){
+
+	var mapOptions = {
+		zoom: 8,
+		center: new google.maps.LatLng(-34.397, 150.644),
+		mapTypeId: google.maps.mapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
