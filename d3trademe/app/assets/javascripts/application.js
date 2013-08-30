d7ad752b8e35024012587aa8c5f94647bdb425a7
maxdgodfrey <maxdgodfrey@gmail.com>
2013-08-31 11:07:48 +1200

    added some more google stuff, should almost work need css

--- d3trademe/app/assets/javascripts/application.js
+++ d3trademe/app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ function initialize(){
 	var mapOptions = {
 		zoom: 8,
 		center: new google.maps.LatLng(-34.397, 150.644),
-		mapTypeId: google.maps.mapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 }
