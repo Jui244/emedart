ae9bd2829f38343fa3824145c495a7f5cb94d867
Jui244 <jui@deshpande.net.nz>
2013-08-31 09:40:04 +1200

    Start

---
+++ d3trademe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3trademe::Application.config.secret_key_base = '0d03277daef18b0a5195997969c1be68290cac063dc690f551957ebf0fd4bc770ef15c08768c6258c0cfc27204078cf2c20d1a3d88915826525d531fd139560d'
---
+++ d3trademe/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3trademe::Application.config.session_store :cookie_store, key: '_d3trademe_session'
