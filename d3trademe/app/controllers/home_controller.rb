0469123ceb1128a3a9b253c564224252c10d693e
maxdgodfrey <maxdgodfrey@gmail.com>
2013-08-31 13:17:28 +1200

    added a few locations to the fake JSON object/ruby array for testing search

--- d3trademe/app/controllers/home_controller.rb
+++ d3trademe/app/controllers/home_controller.rb
@@ -6,7 +6,21 @@ class HomeController < ApplicationController
 		params[:keywords]
 
 		
-		fakeData = [SearchResult.new("Marlborough", 300)]
+		fakeData = [SearchResult.new("Marlborough", 300),
+			SearchResult.new("Wellington", 1000),
+			SearchResult.new("Auckland", 1700),
+			SearchResult.new("Waikato", 400),
+			SearchResult.new("Gisborne", 150),
+			SearchResult.new("Otago",678 ),
+			SearchResult.new("Taranaki", 70),
+			SearchResult.new("Wairarapa", 50),
+			SearchResult.new("Nelson Bays", 300),
+			SearchResult.new("Hawke's Bay", 60),
+			SearchResult.new("Wanganui", 30),
+			SearchResult.new("West Coast", 600),
+			SearchResult.new("Canterbury", 60),
+
+		]
 		
 		render json: fakeData
 	end
