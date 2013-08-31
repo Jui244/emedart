3378d9508fd720e3e213293d777abe35df99d74b
ImaPotato <scottallen@Scott-Allens-MacBook-Pro.local>
2013-08-31 13:41:18 +1200

    more changes

--- d3trademe/scraper.rb
+++ d3trademe/scraper.rb
@@ -9,14 +9,14 @@ require 'sqlite3'
 
 def generate(cat)
 begin
-	db = SQLite3::Database.new "database.db3"
+	db = SQLite3::Database.new "db/development.sqlite3"
 
 	url = "http://api.trademe.co.nz/v1/Categories/#{cat}.json"
 	result = JSON.parse(RestClient.get(url))
     db.execute"DROP TABLE IF EXISTS temp_categories"
-	db.execute"CREATE TABLE IF NOT EXISTS temp_categories( STRING, number STRING PRIMARY KEY, path STRING)" 
-	result["Subcategories"].each do |subcategory|
-		db.execute "INSERT INTO temp_categories values (?, ?, ?)", [subcategory["Name"], subcategory["Number"], subcategory["Path"]]
+	db.execute"CREATE TABLE IF NOT EXISTS temp_categories(id INTEGER,  STRING, number STRING PRIMARY KEY, path STRING)" 
+	result["Subcategories"].each_with_index do |subcategory, index|
+		db.execute "INSERT INTO temp_categories values (?, ?, ?, ?)",[index, subcategory["Name"], subcategory["Number"], subcategory["Path"]]
 	end
 rescue SQLite3::Exception => e 
     puts "Exception occured"
@@ -31,13 +31,13 @@ url = 'http://api.trademe.co.nz/v1/Categories/0.json'
 result = JSON.parse(RestClient.get(url))
 
 begin
-db = SQLite3::Database.open "database.db3"
+db = SQLite3::Database.open "db/development.sqlite3"
 
     db.execute"DROP TABLE IF EXISTS categories"
-	db.execute"CREATE TABLE IF NOT EXISTS categories(name STRING, number STRING PRIMARY KEY, path STRING)"  
+	db.execute"CREATE TABLE IF NOT EXISTS categories(id INTEGER, name STRING, number STRING PRIMARY KEY, path STRING)"  
 
-	result["Subcategories"].each do |subcategory|
-		db.execute "INSERT INTO categories values (?, ?, ?)", [subcategory["Name"], subcategory["Number"], subcategory["Path"]]
+	result["Subcategories"].each_with_index do |subcategory, index|
+		db.execute "INSERT INTO categories values (?, ?, ?, ?)", [index, subcategory["Name"], subcategory["Number"], subcategory["Path"]]
 	end
 
 rescue SQLite3::Exception => e 
