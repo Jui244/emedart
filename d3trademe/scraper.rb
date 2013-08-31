2a258c58117ae4b35d1aaf7679afe08889919f86
ImaPotato <scottallen@Scott-Allens-MacBook-Pro.local>
2013-08-31 13:34:59 +1200

    new database

--- d3trademe/scraper.rb
+++ d3trademe/scraper.rb
@@ -6,15 +6,17 @@ require 'json'
 require 'net/http'
 require 'sqlite3'
 
+
 def generate(cat)
 begin
+	db = SQLite3::Database.new "database.db3"
+
 	url = "http://api.trademe.co.nz/v1/Categories/#{cat}.json"
 	result = JSON.parse(RestClient.get(url))
-	db = SQLite3::Database.new "databaseTemp.db3"
-    db.execute"DROP TABLE IF EXISTS Categories"
-	db.execute"CREATE TABLE IF NOT EXISTS Categories(NAME STRING, NUMBER STRING PRIMARY KEY, PATH STRING)" 
+    db.execute"DROP TABLE IF EXISTS temp_categories"
+	db.execute"CREATE TABLE IF NOT EXISTS temp_categories( STRING, number STRING PRIMARY KEY, path STRING)" 
 	result["Subcategories"].each do |subcategory|
-		db.execute "INSERT INTO Categories values (?, ?, ?)", [subcategory["Name"], subcategory["Number"], subcategory["Path"]]
+		db.execute "INSERT INTO temp_categories values (?, ?, ?)", [subcategory["Name"], subcategory["Number"], subcategory["Path"]]
 	end
 rescue SQLite3::Exception => e 
     puts "Exception occured"
@@ -29,13 +31,13 @@ url = 'http://api.trademe.co.nz/v1/Categories/0.json'
 result = JSON.parse(RestClient.get(url))
 
 begin
-    
-    db = SQLite3::Database.new "database.db3"
-    db.execute"DROP TABLE IF EXISTS Categories"
-	db.execute"CREATE TABLE IF NOT EXISTS Categories(NAME STRING, NUMBER STRING PRIMARY KEY, PATH STRING)"  
+db = SQLite3::Database.open "database.db3"
+
+    db.execute"DROP TABLE IF EXISTS categories"
+	db.execute"CREATE TABLE IF NOT EXISTS categories(name STRING, number STRING PRIMARY KEY, path STRING)"  
 
 	result["Subcategories"].each do |subcategory|
-		db.execute "INSERT INTO Categories values (?, ?, ?)", [subcategory["Name"], subcategory["Number"], subcategory["Path"]]
+		db.execute "INSERT INTO categories values (?, ?, ?)", [subcategory["Name"], subcategory["Number"], subcategory["Path"]]
 	end
 
 rescue SQLite3::Exception => e 
