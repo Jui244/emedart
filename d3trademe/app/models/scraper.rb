942be78d11fc975cd7f1ce1a3bbe9624f944467a
Peter Bennie <peter@BMO.(none)>
2013-08-31 14:20:24 +1200

    moved Scraper to a class

---
+++ d3trademe/app/models/scraper.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'crack'
+require 'open-uri'
+require 'rest-client'
+require 'json'
+require 'net/http'
+require 'sqlite3'
+
+
+class Scraper
+
+def self.generate(cat)
+
+	url = "http://api.trademe.co.nz/v1/Categories/#{cat}.json?depth=1"
+	result = JSON.parse(RestClient.get(url))
+
+	return result["Subcategories"]
+end
+
+
+def self.initialise
+
+url = 'http://api.trademe.co.nz/v1/Categories/0.json'
+
+result = JSON.parse(RestClient.get(url))
+begin
+#db = SQLite3::Database.new "../../db/development.sqlite3"
+db = Category.connection
+
+    db.execute"DROP TABLE IF EXISTS categories"
+	db.execute"CREATE TABLE IF NOT EXISTS categories(id INTEGER, name STRING, number STRING PRIMARY KEY, path STRING)"  
+
+	result["Subcategories"].each_with_index do |subcategory, index|
+		db.execute "INSERT INTO categories values (?, ?, ?, ?)", [index, subcategory["Name"], subcategory["Number"], subcategory["Path"]]
+	end
+
+rescue SQLite3::Exception => e 
+    
+    puts "Exception occured"
+    puts e  
+    
+ensure
+    db.close if db
+end
+end
+end
+
