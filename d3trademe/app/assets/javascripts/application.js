cff6deff372e653e162daf3104306abc4bd2569b
Jui244 <jui@deshpande.net.nz>
2013-08-31 12:15:03 +1200

    Search bar

--- d3trademe/app/assets/javascripts/application.js
+++ d3trademe/app/assets/javascripts/application.js
@@ -14,6 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+window.onload = function(){ 
+	//Get submit button
+	var submitbutton = document.getElementById("tfq");
+	//Add listener to submit button
+	if(submitbutton.addEventListener){
+		submitbutton.addEventListener("click", function() {
+			if (submitbutton.value == 'Search our website'){//Customize this text string to whatever you want
+				submitbutton.value = '';
+			}
+		});
+	}
+}
+
 var map;
 function initialize(){
 
